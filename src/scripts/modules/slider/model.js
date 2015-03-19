0975eb86b57bbde89c73cd96179094b2908bdebb
Warren <w.green@hotmail.com>
2015-03-19 21:09:46 +0000

    updated

--- src/scripts/modules/slider/model.js
+++ src/scripts/modules/slider/model.js
@@ -3,12 +3,12 @@ module.exports = function sliderModel() {
   var dir = "/images/projects";
   var fileextension = ".jpg";
 
-  var numImages = 15;
+  // var numImages = 34;
+  var numImages = 2;
 
   var domNode = document.querySelector('[data-behaviour="carousel-slider"]');
 
   var docFrag = document.createDocumentFragment();
-
 
 
   for(var i = 1; i <= numImages; i += 1) {
@@ -20,6 +20,17 @@ module.exports = function sliderModel() {
     sliderItemContainer.appendChild(img);
     docFrag.appendChild(sliderItemContainer);
   }
+
+
+  // for(var i = 1; i <= numImages; i += 1) {
+
+  //   var sliderItemContainer = document.createElement('div');
+  //   var img = document.createElement('img');
+  //   img.setAttribute('src', '/images/projects/project-image-' + i + '.jpg');
+
+  //   sliderItemContainer.appendChild(img);
+  //   docFrag.appendChild(sliderItemContainer);
+  // }
 
 
   domNode.appendChild(docFrag);
