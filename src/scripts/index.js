bca060c82c3b6089f6d2efb22052d3758d92cbb2
Warren <w.green@hotmail.com>
2016-10-16 20:01:53 +0100

    updated css to scale images on hover

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,36 +3,9 @@ var $ = require('jquery');
 
 $(function(){
 
-  var Menu = require('./modules/menu');
-  var Nav = require('./modules/nav');
   var layout = require('./modules/layout');
-  var DividerCirclesPulse = require('./modules/divider-circles-pulse');
-
-
-  $('.carousel-slider').slick({
-    dots : true,
-    fade : true,
-    cssEase : 'linear'
-  });
-
-
-  var menuEl = $('#js-menu-icon');
-  var menuModule = new Menu(menuEl);
-  menuModule.init();
-
-
-  // var navEl = $('#main-nav-container-js');
-  // var navModule = new Nav(navEl);
-  // navModule.init();
-
   var dividers = $('.divider-one');
+
   layout(dividers).init();
-
-  var dividersCircles = $('.divider__circle');
-  $.each(dividersCircles, function(index, circle) {
-    var module = new DividerCirclesPulse(circle);
-    module.init();
-  });
-
 
 });
--- src/scripts/modules/layout.js
+++ src/scripts/modules/layout.js
@@ -2,7 +2,7 @@ module.exports = function layout(subSections) {
 
   return {
     init : function initFn() {
-      subSections.height(window.innerHeight + 'px');
+      subSections.height(window.innerHeight - 210 + 'px'); // 210 = height of mobile nav
     }
 
   };
